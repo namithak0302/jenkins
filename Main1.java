c6c535472a56a365ca7654c42409f1720502a334
Namitha K <namithaknamithak7@gmail.com>
2025-10-29 16:18:56 +0530

    Update Main1.java

--- Main1.java
+++ Main1.java
@@ -1,5 +1,5 @@
-public class Main{
-    public static void main(String args[])
+public class Main1{
+    public static void main(String[] args)
     {
         System.out.println("hello world");
     }
